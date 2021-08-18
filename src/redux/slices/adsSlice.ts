fd2635121488de66e275919ac68a8721346f5c69
Synthia Hanitriniaina <Bakozetra>
2021-08-18 09:33:38 +0300

    Feat(Fectch-category) Fetching categories and display it on the homepage

--- src/redux/slices/adsSlice.ts
+++ src/redux/slices/adsSlice.ts
@@ -54,7 +54,6 @@ export const fetchAds = createAsyncThunk<
         message: 'Failed to fetch todos.',
       })
     }
-
     return ads
   }
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,23 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import adsReducer from "./slices/adsSlice";
-import { createBrowserHistory } from "history";
-import { connectRouter, routerMiddleware } from "connected-react-router";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from '@reduxjs/toolkit'
+import adsReducer from './slices/adsSlice'
+import categoriesReducer from './slices/categoriesSlice'
+import { createBrowserHistory } from 'history'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { configureStore } from '@reduxjs/toolkit'
 
-export const history = createBrowserHistory();
+export const history = createBrowserHistory()
 
 const rootReducer = combineReducers({
   router: connectRouter(history) as any,
   ads: adsReducer,
-});
+  categories: categoriesReducer,
+})
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(routerMiddleware(history)),
-});
+})
 
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
